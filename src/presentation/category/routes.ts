de4d3449d8e40a3c385f1302ffccf31300fc0366
Salvador Bravo <sa.bravo0901@gmail.com>
2026-01-12 10:57:46 -0300

    feat: integrar el middleware en las rutas de los controladores

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoryController } from "./controller";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
 
 export class CategoryRoutes {
   static get routes(): Router {
@@ -8,7 +9,11 @@ export class CategoryRoutes {
 
     // Definir las rutas
     router.get("/", categoryController.getCategories);
-    router.post("/", categoryController.createCategory);
+    router.post(
+      "/",
+      [AuthMiddleware.validateJWT],
+      categoryController.createCategory
+    );
 
     return router;
   }
