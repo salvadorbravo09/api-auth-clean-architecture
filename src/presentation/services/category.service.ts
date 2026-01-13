4e02b1c1d0e4fcbe9f3573a7dc5586f55acb3e86
Salvador Bravo <sa.bravo0901@gmail.com>
2026-01-13 15:51:58 -0300

    feat: refactorizar el servicio de categorías para mejorar la estructura y agregar soporte de paginación

--- src/presentation/services/category.service.ts
+++ src/presentation/services/category.service.ts
@@ -1,15 +1,22 @@
 import { CategoryModel } from "../../data";
-import { CreateCategoryDto, CustomError, UserEntity } from "../../domain";
+import {
+  CreateCategoryDto,
+  CustomError,
+  PaginationDto,
+  UserEntity,
+} from "../../domain";
 
 export class CategoryService {
   // DI
   constructor() {}
 
   async createCategory(createCategoryDto: CreateCategoryDto, user: UserEntity) {
+    // Verificar si la categoria ya existe
     const categoryExists = await CategoryModel.findOne({
       name: createCategoryDto.name,
     });
 
+    // Si existe, lanzar un error
     if (categoryExists) {
       throw CustomError.badRequest("Category already exists");
     }
@@ -22,10 +29,44 @@ export class CategoryService {
 
       await category.save();
 
+      // Rettornar la categoria creada en el formato JSON esperado
       return {
         id: category.id,
         name: category.name,
         available: category.available,
+      };
+    } catch (error) {
+      throw CustomError.internalServerError(`${error}`);
+    }
+  }
+
+  async getCategories(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    try {
+      // const total = await CategoryModel.countDocuments();
+      // const categories = await CategoryModel.find()
+      //   .skip((page - 1) * limit)
+      //   .limit(limit);
+
+      const [total, categories] = await Promise.all([
+        CategoryModel.countDocuments(),
+        CategoryModel.find()
+          .skip((page - 1) * limit)
+          .limit(limit),
+      ]);
+
+      return {
+        page: page,
+        limit: limit,
+        total: total,
+        categories: categories.map((category) => {
+          return {
+            id: category.id,
+            name: category.name,
+            available: category.available,
+          };
+        }),
       };
     } catch (error) {
       throw CustomError.internalServerError(`${error}`);
