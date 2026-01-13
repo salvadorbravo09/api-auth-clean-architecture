b2762cc7e3ed740f537fc8415694d2205f29da12
Salvador Bravo <sa.bravo0901@gmail.com>
2026-01-13 15:53:03 -0300

    feat: implementar soporte para paginacion en metodo getCategories

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { CreateCategoryDto, CustomError } from "../../domain";
+import { CreateCategoryDto, CustomError, PaginationDto } from "../../domain";
 import { CategoryService } from "../services/category.service";
 
 export class CategoryController {
@@ -31,6 +31,21 @@ export class CategoryController {
   };
 
   getCategories = async (req: Request, res: Response) => {
-    res.json("obtener categorias");
+    const { page = 1, limit = 10 } = req.query;
+    const [error, paginationDto] = PaginationDto.create(+page, +limit);
+
+    if (error) {
+      return res.status(400).json({ error });
+    }
+
+
+    this.categoryService
+      .getCategories(paginationDto!)
+      .then((categories) => {
+        return res.status(200).json(categories);
+      })
+      .catch((error) => {
+        return this.handleError(error, res);
+      });
   };
 }
